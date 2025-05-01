8373cc10acf635fe7a700657da5e91c3dceee72b
User Name <davolastagg@gmail.com>
2025-04-30 22:25:33 -0500

    update

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+/* User stories are crucial in breaking down the features of an application and structuring the development process. They are typically written as “As a user, I want to…”, which helps in focusing on user needs and experiences.         Preliminary Questions
+
+When planning our code, we might consider the following:
+
+1. How do we capture the “value” when a number button is clicked?
+2. What steps are needed to achieve this?
+3. Can we use existing attributes to tie events to our number button elements?
+
+
+Here are the user stories for this lab:
+
+As a user, I want to be able to select numbers so that I can perform operations with them.
+As a user, I want to be able to add two numbers together.
+As a user, I want to be able to subtract one number from another.
+As a user, I want to be able to multiply two numbers together.
+As a user, I want to be able to divide one number by another.
+As a user, I want to be able to see the output of the mathematical operation.
+As a user, I want to be able to clear all operations and start from 0. */
+
+
 /*-------------------------------- Constants --------------------------------*/
 
 /*-------------------------------- Variables --------------------------------*/
